bfefdfef2d33bd81bd01f5a00fd9488c21d29079
fripig <fripig@gmail.com>
2017-05-26 18:26:17 +0800

    check getStatusCode

---
+++ fetchAll.php
@@ -0,0 +1,46 @@
+<?php
+require 'vendor/autoload.php';
+
+$file = $argv[1] ?? 'q/pickup.json' ;
+
+$all_q = file_get_contents('q/pickup.json');
+
+$all_q = json_decode($all_q,true);
+
+foreach($all_q as $q)
+{
+    $gcs = new \App\GCSFile();
+    $LINEQ = new \App\LINEQ($gcs);
+    echo $q['link']."\n";
+    $temp = explode('/',$q['link']);
+    if(count($temp) >1 && $temp[1] == 'q'){
+        $id = $temp[2];
+        $data = $LINEQ->fetchQ($id);
+        if($data){
+            file_put_contents('q/single/'.$id.'.json',json_encode($data));
+        }
+
+    }
+    if(count($temp) >1 && $temp[1] == 'note'){
+        $id = $temp[2];
+        $data = $LINEQ->fetchNote($id);
+        if($data) {
+            file_put_contents('note/' . $id . '.json', json_encode($data));
+        }
+    }
+    if(count($temp) >1 && $temp[2] == 'q'){
+        $id = $temp[3];
+        $data = $LINEQ->fetchQ($id);
+        if($data) {
+            file_put_contents('q/single/' . $id . '.json', json_encode($data));
+        }
+    }
+    if(count($temp) >1 && $temp[2] == 'note'){
+        $id = $temp[3];
+        $data = $LINEQ->fetchNote($id);
+        if($data) {
+            file_put_contents('note/' . $id . '.json', json_encode($data));
+        }
+    }
+}
+
